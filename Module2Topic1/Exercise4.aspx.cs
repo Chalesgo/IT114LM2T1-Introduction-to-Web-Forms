b877dade57133a8e752c3fec0de3af56fea39b22
Chalesgo <150835564+Chalesgo@users.noreply.github.com>
2024-02-27 02:49:23 +0800

    Update Exercise4.aspx.cs

--- Module2Topic1/Exercise4.aspx.cs
+++ Module2Topic1/Exercise4.aspx.cs
@@ -9,9 +9,15 @@ namespace Module1Exercise1
 {
     public partial class Exercise4 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void SubmitForm(object sender, EventArgs e)
         {
-
+            if (Page.IsValid)
+            {
+                result.Text = $"Full Name: {fullName.Text}<br />" +
+                              $"Age: {age.Text}<br />" +
+                              $"Email: {email.Text}<br />" +
+                              $"Confirm Email: {confirmEmail.Text}";
+            }
         }
     }
 }
